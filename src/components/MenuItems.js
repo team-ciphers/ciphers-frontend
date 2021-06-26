5d36257941d19d796dbcbcffc4058132d04e21cc
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-27 01:46:58 +0300

    desigen header

---
+++ src/components/MenuItems.js
@@ -0,0 +1,25 @@
+export const menuItems=[
+    {
+        title:'Home',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'Profile',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'About Us',
+        url:'#',
+        cName:'nav-links'
+    }, 
+     {
+        title:'Contact Us',
+        url:'#',
+        cName:'nav-links'
+    }
+  
+]
+
+    
