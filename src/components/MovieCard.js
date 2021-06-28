e6f7b2ffd46d892118dcb6c6b0b98198eb569cf5
root <root@STDLTUC-L069.localdomain>
2021-06-28 16:19:45 +0300

    add favorite an watch button

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -16,26 +16,48 @@ export class MovieCard extends Component {
             vote_average: '',
             overview: '',
             release_date: '',
+            movieId: '',
             reviews: [],
+            url: '',
         }
     }
 
-    handleClose = () => this.setState({ show: false });
-    handleShow = async (original_title, overview, release_date, vote_average, poster_path, id) => {
 
-        // await axios.get(`${process.env.REACT_APP_SERVER_URL}/userReviews?movieId=${id}`).then(response => {
-        this.setState({
-            // reviews: response.data,
-            show: !this.state.show,
-            original_title: original_title,
-            poster_path: poster_path,
-            vote_average: vote_average,
-            overview: overview,
-            release_date: release_date
-        })
-        // console.log(response.data);
-        // }).catch(error => error.message);
-        // console.log('title', this.state.original_title);
+    handleClose = () => this.setState({ show: false });
+    handleShow = async (original_title, overview, release_date, vote_average, poster_path, movieId) => {
+
+        await axios.get(`${process.env.REACT_APP_SERVER_URL}/userReviews?movieId=${movieId}`).then(response => {
+            this.setState({
+                reviews: response.data,
+                show: !this.state.show,
+                original_title: original_title,
+                poster_path: poster_path,
+                vote_average: vote_average,
+                overview: overview,
+                release_date: release_date,
+                movieId: movieId
+            })
+        }).catch(error => {
+            this.setState({
+
+                show: !this.state.show,
+                original_title: original_title,
+                poster_path: poster_path,
+                vote_average: vote_average,
+                overview: overview,
+                release_date: release_date,
+                movieId: movieId
+            })
+        });
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/movieTrailer?movieId=${movieId}`).then(response => {
+            console.log('cvbnm,', response.data);
+            this.setState({
+                url: response.data,
+
+            })
+
+        }).catch(error => alert(error))
+
     };
     render() {
         return (
@@ -49,13 +71,16 @@ export class MovieCard extends Component {
                     overview={this.state.overview}
                     release_date={this.state.release_date}
                     reviews={this.state.reviews}
+                    movieId={this.state.movieId}
+                    url={this.state.url}
+
                 />
 
                 <Row xs={1} md={3} className="g-4">
                     {this.props.searchMovie.map(item => {
                         return (<Col>
-                            <Card onClick={() => this.handleShow(item.original_title, item.overview, item.release_date, item.vote_average, item.poster_path, item.id)}>
-                                <Card.Body>
+                            <Card onClick={() => this.handleShow(item.original_title, item.overview, item.release_date, item.vote_average, item.poster_path, item.movieId)} >
+                                <Card.Body >
                                     <Card.Img variant="top" src={item.poster_path} />
                                     <Card.Title style={{ fontSize: '30px' }}>{item.original_title}</Card.Title>
                                 </Card.Body>
