c68b044b5af51121096a3b36fbbacab01a676abe
saify96 <mohammad.alsaify@gmail.com>
2021-06-28 13:41:04 +0300

    profile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
+import Home from './components/Home';
+import Profile from './components/Profile';
+
+
 
 ReactDOM.render(
   <Auth0Provider
@@ -10,7 +21,17 @@ ReactDOM.render(
     clientId={process.env.REACT_APP_CLIENT_ID}
     redirectUri={window.location.origin}
   >
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={props => <Home {...props} />} />
+        <Route path="/Profile" exact render={props => <Profile {...props} />} />
+        {/* <Route path="/Aboutus" exact render={props => <Profile {...props} />} /> */}
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>,
   </Auth0Provider>,
   document.getElementById('root')
 );
+
+
